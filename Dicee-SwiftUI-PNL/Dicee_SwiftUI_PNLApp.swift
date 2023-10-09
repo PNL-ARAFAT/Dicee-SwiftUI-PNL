90dfd9907553d35f9c91bdc2b4d8ef57fea473af
arafat2183 <akash076987@gmail.com>
2023-10-09 21:03:22 +0900

    Initial Commit

---
+++ Dicee-SwiftUI-PNL/Dicee_SwiftUI_PNLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUI_PNLApp.swift
+//  Dicee-SwiftUI-PNL
+//
+//  Created by Akash Arafat on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUI_PNLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
